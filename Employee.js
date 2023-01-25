c0c07134629054889bab649325bc0caf0ee95f8a
qismat <qismatnisha6243@gmail.com>
2023-01-25 21:59:27 +0530

    [qismat] Add :UC12-AddingStartingDate

--- Employee.js
+++ Employee.js
@@ -1,13 +1,18 @@
 class EmployePayrollData
-{//UC11
+{//UC12
     id;
     salary;
+    gender;
+    startDate;
+    gender;
     
-    constructor(id,name,salary)
+    constructor(id,name,salary,gender,startdata)
     {
         this.id=id;
         this.name=name;
         this.salary=salary;
+        this.gender=gender;
+        this.startDate=startDate;
        
 
 
@@ -26,13 +31,17 @@ class EmployePayrollData
     tostring()
     {
        
-        return "id = "+this.id+", name= "+this.name+", gender = "+this.salary;
+        const options ={year:'numeric',month:'long',day:'numaric'};
+        const empdate=this.startDate==undefined?"underfined":
+        this.startDate.toLocalDateString(empdate,options);
+        return id = "+this.id+", name= "+this.name+",salary=" +this.salary+ ", gender = "+this.gender+", startDate = "+empdate+";
 
 
     }
+
 }
 
-    let employePayrollData=new EmployePayrollData(1,"mark",2300);
+    let employePayrollData=new EmployePayrollData(1,"mark",2300,"f",23/06/2000);
     console.log(employePayrollData.tostring());
 
 
